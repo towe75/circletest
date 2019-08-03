87a589848a81fe289808f508e93258b9f39bfd33
Thomas Weber <thomas.weber@pascom.net>
2019-08-03 17:13:18 +0200

    go test

--- test.sh
+++ test.sh
@@ -8,7 +8,13 @@ apt-get install -y liblxc1 lxc-dev lxc lxc-templates wget ca-certificates
 
 lxc-ls -f
 
+go version
+
+
 cd /usr/local
+
+ls -l
+
 wget -O- https://storage.googleapis.com/golang/go1.12.7.linux-amd64.tar.gz| tar xfz -
 ln -s /usr/local/go/bin/go /usr/bin/go
 
