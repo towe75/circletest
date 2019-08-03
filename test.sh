14a672c38c8558e1209586774bcb4c35a0da41b1
Thomas Weber <thomas.weber@pascom.net>
2019-08-03 16:41:15 +0200

    add go

--- test.sh
+++ test.sh
@@ -4,9 +4,12 @@ set -o errexit
 
 apt-get update || true
 
-apt-get install -y liblxc1 lxc-dev lxc lxc-templates
+apt-get install -y liblxc1 lxc-dev lxc lxc-templates wget ca-certificates
 
 lxc-ls -f
 
-docker pull golang:1.12.7
-docker ps -a
+cd /usr/local
+wget -O- https://storage.googleapis.com/golang/go1.12.7.linux-amd64.tar.gz| tar xfz -
+ln -s /usr/local/go/bin/go /usr/bin/go
+
+go version
